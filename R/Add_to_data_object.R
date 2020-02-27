38560d89b05cdefe6ac908b8f460a1595b1bedae
Michael Krabbe Borregaard <mkborregaard@snm.ku.dk>
2020-02-27 13:22:03 +0100

    white space with `if`

--- R/Add_to_data_object.R
+++ R/Add_to_data_object.R
@@ -1,15 +1,15 @@
 # I made some major changes here and to infer_sites, that also need to be done for species!
 add_sitestat <- function(distrib_data, site_stat, site = NULL){
-  if(!inherits(distrib_data, "distrib_data"))
+  if (!inherits(distrib_data, "distrib_data"))
     stop("distrib_data must be an object of type distrib_data, nodiv_data or nodiv_result")
 
-  if(is.matrix(site_stat))
+  if (is.matrix(site_stat))
     site_stat <- as.data.frame(site_stat, stringsAsFactors = FALSE)
 
-  if(is.vector(site_stat)){
+  if (is.vector(site_stat)) {
     nam <- deparse(substitute(site_stat))
-    if(is.null(names(site_stat)) & length(site_stat) == Nsites(distrib_data)){
-      if(nam %in% names(distrib_data$coords@data))
+    if (is.null(names(site_stat)) & length(site_stat) == Nsites(distrib_data)) {
+      if (nam %in% names(distrib_data$coords@data))
         warning(paste("Overwriting the contents of", nam))
       distrib_data$coords@data[[nam]] <- site_stat
       return(distrib_data)
@@ -32,19 +32,19 @@ add_sitestat <- function(distrib_data, site_stat, site = NULL){
 
   site <- suppressWarnings(identify_sites(as.character(site), distrib_data))
 
- # if(length(site) < Nsites(distrib_data))
+ # if (length(site) < Nsites(distrib_data))
   #  cat(paste(Nsites(distrib_data)- length(site), "sites from site_stat were not found in", deparse(substitute(distrib_data)), "\n"))
 
   mergeframe <- as.data.frame(lapply(site_stat, function(column) {
     ret <- vector(mode = typeof(column), length = Nsites(distrib_data))
     ret[] <- NA
-    if(is.factor(column))
+    if (is.factor(column))
       ret <- factor(ret, levels = levels(column))
     ret[site] <- column
     ret
   }), stringsAsFactors = FALSE)
 
-  if(sum(names(mergeframe) %in% names(distrib_data$coords@data)) > 0){
+  if (sum(names(mergeframe) %in% names(distrib_data$coords@data)) > 0) {
     matches <- which(names(distrib_data$coords@data) %in% names(mergeframe))
     deleted <- names(distrib_data$coords@data)[matches]
     distrib_data$coords <- distrib_data$coords[, -matches]
@@ -60,15 +60,15 @@ add_species_stat <- function(distrib_data, species_stat, specs = NULL){
   #restorepoint::restore.point("add_species_stat")
 
 
-  if(!inherits(distrib_data, "distrib_data"))
+  if (!inherits(distrib_data, "distrib_data"))
     stop("distrib_data must be an object of type distrib_data, nodiv_data or nodiv_result")
-  if(is.null(distrib_data$species_stats))
+  if (is.null(distrib_data$species_stats))
     stop("The distrib_data object is from an earlier version of nodiv. Please run update_object on the object before proceeding")
 
-  if(is.matrix(species_stat))
+  if (is.matrix(species_stat))
     species_stat <- as.data.frame(species_stat, stringsAsFactors = FALSE)
 
-  if(is.vector(species_stat)){
+  if (is.vector(species_stat)) {
     nam <- deparse(substitute(species_stat))
     species_stat <- as.data.frame(species_stat, stringsAsFactors = FALSE)
     names(species_stat) <- nam
@@ -76,7 +76,7 @@ add_species_stat <- function(distrib_data, species_stat, specs = NULL){
 
   num <- nrow(species_stat)
 
-  if(is.null(specs))
+  if (is.null(specs))
   {
     temp <- infer_species(distrib_data, species_stat)
     specs <- temp$species
@@ -85,18 +85,18 @@ add_species_stat <- function(distrib_data, species_stat, specs = NULL){
 
   specs <- identify_species(specs, distrib_data)
 
-  if(length(specs) < Nspecies(distrib_data))
+  if (length(specs) < Nspecies(distrib_data))
     cat(paste(num - length(specs), "species were not found in", deparse(substitute(distrib_data)), "\n"))
 
   mergeframe <- as.data.frame(lapply(species_stat, function(column) {
     ret <- vector(mode = typeof(column), length = Nspecies(distrib_data))
     ret[] <- NA
-    if(is.factor(column)) ret <- factor(ret, levels = levels(column))
+    if (is.factor(column)) ret <- factor(ret, levels = levels(column))
     ret[specs] <- column
     ret
   }), stringsAsFactors = FALSE)
 
-  if(sum(names(mergeframe) %in% names(distrib_data$species_stats)) > 0){
+  if (sum(names(mergeframe) %in% names(distrib_data$species_stats)) > 0) {
     matches <- which(names(distrib_data$species_stats) %in% names(mergeframe))
     deleted <- names(distrib_data$species_stats)[matches]
     distrib_data$species_stats[,matches] <- NULL
@@ -113,9 +113,9 @@ add_species_stat <- function(distrib_data, species_stat, specs = NULL){
 infer_sites_intern <- function(sites, site_stat) # a non-exported convenience function
 {
 #   suppressWarnings(numsites <- as.numeric(sites)) #I removed this as it caused trouble
-#   if(sum(is.na(numsites)) < 0.2*length(sites)){
-#     if(all.equal(numsites, floor(numsites))) {      # if site names are just integers, matching is not attempted
-#       if(nrow(site_stat) == length(sites))
+#   if (sum(is.na(numsites)) < 0.2*length(sites)){
+#     if (all.equal(numsites, floor(numsites))) {      # if site names are just integers, matching is not attempted
+#       if (nrow(site_stat) == length(sites))
 #         return(list(site = sites, site_stat = site_stat)) else
 #           warning("Site matching was done based on name matching, which is tricky when site names are integer values")
 #     }
@@ -180,11 +180,11 @@ infer_sites_intern <- function(sites, site_stat) # a non-exported convenience fu
 
   cat(paste("Matching sites by", name, "\n"))
   hits = sum(site %in% sites)
-  if(hits == length(site) & hits == length(site))
+  if (hits == length(site) & hits == length(site))
     cat("All sites matched\n") else
     cat(paste(hits, " sites were matched:\n\t", floor(hits/length(site) * 100), "% of ", length(site), " sites in site_stat\n\t", floor(hits/length(sites) * 100), "% of ", length(sites), " sites in distrib_data\n", sep = ""))
 
-  if(name == "rownames"){
+  if (name == "rownames"){
     site_stat_ret$sites <- as.character(site)[match(sites, as.character(site))]
     name <- "sites"
   }
@@ -194,7 +194,7 @@ infer_sites_intern <- function(sites, site_stat) # a non-exported convenience fu
 
   names(site_stat_ret)[index] <- "sites"
 
-  if(sum(names(site_stat_ret) == "sites") > 1)
+  if (sum(names(site_stat_ret) == "sites") > 1)
     stop(paste("Could not match on the variable called sites, as", name, "had a greater correspondence. Please rename"))
 
   site_stat_ret$sites <- as.character(site_stat_ret$sites)
@@ -225,7 +225,7 @@ match_speciesnames <- function(reference_name, new_name, do_not_match = FALSE){
   new_ll <- sapply(chars, function(x) length(grep(x, new_name, fixed = TRUE)))
   new_char <- chars[which(new_ll == max(new_ll))[1]]
   new_name <- gsub(new_char, ref_char, new_name, fixed = TRUE)
-  if(do_not_match)
+  if (do_not_match)
     return(new_name)
 
   ret <- match(new_name, reference_name)
@@ -243,10 +243,10 @@ infer_species <- function(distrib_data, species_stat) # a non-exported convenien
     name <- names(species_stat)[res]
     spec <- species(distrib_data)[match_speciesnames(species(distrib_data), species_stat[[res]])]
 
-  if(temp[res] < 0.5 & sum(spec %in% species(distrib_data)) < 0.5 * Nspecies(distrib_data))
+  if (temp[res] < 0.5 & sum(spec %in% species(distrib_data)) < 0.5 * Nspecies(distrib_data))
     stop("Species could not be matched automatically, please supply the species argument explicitly")
 
-  if(!name == "rownames")
+  if (!name == "rownames")
     species_stat[[name]] <- NULL
 
   species_stat$rownames <- NULL
